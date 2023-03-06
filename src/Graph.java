457a9cb9b796ba5d9262306c30d0f365c0885756
Andrei Avram <andrei.Avram@student.vinci.Be>
2023-03-06 14:26:36 +0100

    Graph: classe ajoutée + méthodes principales + constructeur

---
+++ src/Graph.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class Graph {
+
+
+    public Graph(File lignes, File troncons) {
+    }
+
+    public void calculerCheminMinimisantNombreTroncons(String arretDepart, String arretArrivee) {
+    }
+
+    public void calculerCheminMinimisantTempsTransport(String arretDepart, String arretArrivee) {
+    }
+}
