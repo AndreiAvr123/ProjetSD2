655bf175f9a271545a665c546b7227b8f397c899
Rekaya Nour <nourrkaya@live.be>
2023-03-13 14:51:05 +0100

    Realisaton Trancons

---
+++ src/Troncons.java
@@ -0,0 +1,14 @@
+public class Troncons {
+    private int numLigne,duree;
+    private String depart,arrivee;
+
+    public Troncons(int numLigne, int duree, String depart, String arrivee) {
+        this.numLigne = numLigne;
+        this.duree = duree;
+        this.depart = depart;
+        this.arrivee = arrivee;
+    }
+
+
+
+}
