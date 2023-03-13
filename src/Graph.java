798b62d1063ccf23de0d06033bdbb22158f3debd
Andrei Avram <andrei.Avram@student.vinci.Be>
2023-03-13 15:48:13 +0100

    fix: Constructeur

--- src/Graph.java
+++ src/Graph.java
@@ -2,14 +2,22 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Graph {
-    private Map<Stations, List<Troncons>> mapStationsTroncons;
+    Set<Stations> listeStations;
+    private Map<Stations, Set<Troncons>> mapStationsTroncons = new HashMap<>();
 
     public Graph(File lignes, File troncons) {
+        listeStations = lireStations(lignes,2);
+        for (int i = 1; i <= listeStations.size(); i++) {
+            Set<Troncons> listeTroncons = lireTroncons(troncons);
 
+
+        }
     }
 
     public void calculerCheminMinimisantNombreTroncons(String arretDepart, String arretArrivee) {
@@ -21,25 +29,49 @@ public class Graph {
 
     /**
      * Lis un fichier et affiche son contenu
-     * @param fichier le fichier à lire
+     * @param fichierLignes le fichier à lire
      */
-    public void lireFichier(File fichier, int index){
+    public Set<Stations> lireStations(File fichierLignes, int index){
+        Set<Stations> listeRetour = new HashSet<Stations>();
+
         try{
-            FileReader fr = new FileReader(fichier);
+            FileReader fr = new FileReader(fichierLignes);
             BufferedReader br = new BufferedReader(fr);
-            StringBuffer sb = new StringBuffer();
             String ligne;
             while((ligne = br.readLine()) != null){
                 String[] lisplit = ligne.split(",");
-                sb.append(lisplit[index]);
-                sb.append('\n');
+                Stations stations = new Stations(lisplit[index]);
+                listeRetour.add(stations);
             }
             fr.close();
-            System.out.println("Contenu du fichier :");
-            System.out.println(sb);
+
         }catch (IOException e){
             e.printStackTrace();
         }
 
+        return listeRetour;
+    }
+
+    // TODO deuxieme paramètre correspondant à un arret de départ du troncons
+
+    public Set<Troncons> lireTroncons(File fichierTroncons){
+        Set<Troncons> listeRetour = new HashSet<Troncons>();
+
+        try{
+            FileReader fr = new FileReader(fichierTroncons);
+            BufferedReader br = new BufferedReader(fr);
+            String ligne;
+            while((ligne = br.readLine()) != null){
+                String[] lisplit = ligne.split(",");
+                Troncons troncons = new Troncons(new Lignes(Integer.parseInt(lisplit[0])),Integer.parseInt(lisplit[1]),lisplit[2], lisplit[3]);
+                listeRetour.add(troncons);
+            }
+            fr.close();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return listeRetour;
     }
 }
--- src/Lignes.java
+++ src/Lignes.java
@@ -1,14 +1,20 @@
 public class Lignes {
     private int identifiant = 1, numero, tmpsAttenteMoyen;
-    private String depart, destination, typeTransport;
+    private Stations depart, destination;
+    private String typeTransport;
 
     public Lignes(int numero, int tmpsAttenteMoyen,
-        String depart, String destination, String typeTransport){
+        Stations depart, Stations destination, String typeTransport){
       this.identifiant++;
       this.numero = numero;
       this.tmpsAttenteMoyen = tmpsAttenteMoyen;
       this.depart = depart;
       this.destination = destination;
       this.typeTransport = typeTransport;
+    }
+
+    // TODO vérifier ce constructeur
+    public Lignes(int numero) {
+      this.numero = numero;
     }
 }
